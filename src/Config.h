8988bbe34aefe909fe100ba109cd3bc602c6e4b7
Magomedcoder <info@magomedcoder.ru>
2022-12-24 21:25:03 +0300

    Added config

---
+++ src/Config.h
@@ -0,0 +1,8 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+#define C_DARK_RED "\x1b[31m"
+#define C_DARK_GREEN "\x1b[32m"
+#define C_DEFAULT "\x1b[0m"
+#define SNAKE_BODY_SYM C_DARK_GREEN "1" C_DEFAULT
+#define FOOD_SYM C_DARK_RED "0" C_DEFAULT
+#endif
